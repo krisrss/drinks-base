fdca0162ebe90390b301c6d5dee6ffca6a1b4c03
krisrss <kristapsrss@gmail.com>
2021-03-02 17:32:40 +0200

    Add searchbar and display it on screen

---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SearchBar = () => {
+    return (
+        <div>
+            <label>Enter term:</label>
+            <br />
+            <input type='text' />
+            <button>Search</button>
+        </div>
+    );
+};
+
+export default SearchBar;
