e73ada6200341d2f4cf9a82a188d4b45161b3efd
krisrss <kristapsrss@gmail.com>
2021-03-17 17:58:02 +0200

    Apply query string on checkbox click

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 
 const Checkbox = ({ filterData }) => {
+    const search = useLocation();
+    const history = useHistory();
+
+    const getChckeboxValue = (event) => {
+        const value = event.target.value;
+        const removeSpace = value.split(' ').join('').toLowerCase();
+        const filterType = removeSpace.split('/').join('');
+        const mainPath = search.pathname;
+        history.push(`${mainPath}?filter=${filterType}`);
+    }
+
     return (
         <label>
-            <input type="checkbox" />
+            <input type="checkbox" value={filterData[0]} onClick={getChckeboxValue} />
             <span>{`${filterData[0]} ${filterData[1]}`}</span>
         </label>
     );
