951b23362fae5f6d0b6340ba151e756a196f1058
krisrss <kristapsrss@gmail.com>
2021-04-02 16:49:15 +0300

    Fix bug that directed to wrong URL

--- src/components/SearchTermBlock.js
+++ src/components/SearchTermBlock.js
@@ -3,20 +3,30 @@ import { useHistory } from 'react-router-dom';
 
 const SearchTermBlock = ({ searchTerm, resetSpinner, resetDrinkList }) => {
     const history = useHistory();
+
     const onClickHandler = (term) => {
         const currentPath = history.location.pathname;
-        const currentUrl = currentPath;
-        const updatedUrl = currentUrl.replaceAll(`/${term}`, "");
+        const cleanedPath = currentPath.replaceAll(`/ingredients`, "");
+        const queryWords = cleanedPath.split('/');
+
+        var index = queryWords.indexOf(term);
+        if (index >= 0) {
+            queryWords.splice(index, 1);
+        }
+
+        const finalQuery = queryWords.join('/');
+
         resetSpinner();
         resetDrinkList();
-        history.push(updatedUrl, { state: currentUrl });
+
+        history.push(`/ingredients${finalQuery}`, { state: currentPath });
 
     }
 
-    const test = Object.values(searchTerm).map((term, index) => {
+    const searchTerms = Object.values(searchTerm).map((term, index) => {
         return (
             <span key={index} onClick={() => onClickHandler(term)} style={{ border: '1px solid black', padding: '4px' }}>
-                {`${term}`}
+                {`${searchTerms}`}
             </span>
         )
     })
