52d3ef4f19a03eabdea45d57bca13e236365c15c
krisrss <kristapsrss@gmail.com>
2021-04-07 22:23:29 +0300

    Add style to navbar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../css/NavigationBar.css';
 
 const NavigationBar = () => {
     return (
-        <div>
-            <Link to={'/'}>
-                <button>Search by name</button>
-            </Link>
-            --------------
-            <Link to={'/ingredients'}>
-                <button>Search by ingredients</button>
+        <div className='NavigationBar'>
+            <Link to={'/'} className='navbutton nav-active'>
+                By name
             </Link>
 
+            <Link to={'/ingredients'} className='navbutton nav-inactive'>
+                By ingredients
+            </Link>
         </div>
     )
 };
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -29,7 +29,7 @@ const SearchBar = () => {
 
     return (
         <div className='SearchBar'>
-            <input value={input} onChange={onChangeHandler} type='text' placeholder='Search drink by keyword...' />
+            <input value={input} onChange={onChangeHandler} type='text' placeholder='Search drinks by keyword...' />
             <Link to={setPath} onClick={onClickHandler} className='button'>
                 SEARCH
             </Link>
