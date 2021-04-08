a79fe0e60500ca346aaa482b6d2007a738a5a0c5
krisrss <kristapsrss@gmail.com>
2021-04-08 20:57:59 +0300

    Add empty suggestions list component

---
+++ src/components/SuggestionsList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SuggestionsList = () => {
+    return (
+        <div>Suggestions List</div>
+    );
+};
+
+export default SuggestionsList;
--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import SearchBar from '../components/SearchBar';
 import NavigationBar from '../components/NavigationBar';
+import SuggestionsList from '../components/SuggestionsList';
 import '../css/WelcomePage.css';
 
 const WelcomePage = () => {
     return (
         <>
-        <div className='logo'>
-            Drink Base Logo Place holder
-        </div>
+            <div className='logo'>
+                Drink Base Logo Place holder
+            </div>
             <div className='jumbotron'>
-                    <div className='title'>What would you like to drink?</div>
-                    <SearchBar />
-                    <NavigationBar />
+                <div className='title'>What would you like to drink?</div>
+                <SearchBar />
+                <NavigationBar />
+                <SuggestionsList />
             </div>
         </>
     );
