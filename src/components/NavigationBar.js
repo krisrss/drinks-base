4adc88a35e8d5eb3c4aa5156fbeb13264bea9e17
krisrss <kristapsrss@gmail.com>
2021-04-08 20:37:43 +0300

    Show which nav button is clicked

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,15 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import '../css/NavigationBar.css';
 
 const NavigationBar = () => {
+    const history = useHistory();
+    const termPresent = history.location.pathname.includes("ingredients");
+
+    const [byName, setByName] = useState('nav-active');
+    const [byIngredients, setByIngredients] = useState('nav-inactive');
+
+    useEffect(() => {
+        const setButtonStatus = () => {
+            if (termPresent === false) {
+                setByName('nav-active');
+                setByIngredients('nav-inactive');
+            }
+            else {
+                setByName('nav-inactive');
+                setByIngredients('nav-active');
+            }
+        }
+        setButtonStatus();
+    }, []);
+
     return (
         <div className='NavigationBar'>
-            <Link to={'/'} className='navbutton nav-active'>
+            <Link to={'/'} className={`navbutton ${byName}`}>
                 By name
             </Link>
 
-            <Link to={'/ingredients'} className='navbutton nav-inactive'>
+            <Link to={'/ingredients'} className={`navbutton ${byIngredients}`}>
                 By ingredients
             </Link>
         </div>
