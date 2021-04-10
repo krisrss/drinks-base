1d46f4786442969fbfca387f04b6300970195b46
krisrss <kristapsrss@gmail.com>
2021-04-10 20:04:27 +0300

    Display correct searchbar for each page

--- src/pages/ApplicationPage.js
+++ src/pages/ApplicationPage.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import NavigationBar from '../components/NavigationBar';
 import DrinksDisplay from '../components/DrinksDisplay';
 import SearchBar from '../components/SearchBar';
+import AutocompleteBar from '../components/AutocompleteBar';
 import WelcomePage from './WelcomePage';
 
 const ApplicationPage = ({ drinksData, unfilteredDrinksData, urlTerm, getClicketTerm, initialData }) => {
+    const history = useHistory();
+    const currentPath = history.location.pathname;
+
+    const setSearchBar = () => {
+        if (currentPath.includes('/ingredients')) {
+            return <AutocompleteBar />
+        }
+        else {
+            return <SearchBar />
+        };
+    };
+
     return (
         <>
             {Object.values(urlTerm).length !== 0 ?
                 <div className='container'>
                     <div style={{ margin: '1em' }} >
-                        <SearchBar />
+                        {setSearchBar()}
                     </div>
 
                     <NavigationBar />
--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -2,11 +2,24 @@ import React from 'react';
 import SearchBar from '../components/SearchBar';
 import NavigationBar from '../components/NavigationBar';
 import SuggestionsList from '../components/SuggestionsList';
-import '../css/WelcomePage.css';
 import AutocompleteBar from '../components/AutocompleteBar';
+import { useHistory } from 'react-router-dom';
+import '../css/WelcomePage.css';
 
 
 const WelcomePage = () => {
+    const history = useHistory();
+    const currentPath = history.location.pathname;
+
+    const setSearchBar = () => {
+        if (currentPath.includes('/ingredients')) {
+            return <AutocompleteBar />
+        }
+        else {
+            return <SearchBar />
+        };
+    };
+
     return (
         <>
             <div className='logo'>
@@ -14,7 +27,7 @@ const WelcomePage = () => {
             </div>
             <div className='jumbotron'>
                 <div className='title'>What would you like to drink?</div>
-                <AutocompleteBar />
+                {setSearchBar()}
                 <NavigationBar />
                 <SuggestionsList />
             </div>
