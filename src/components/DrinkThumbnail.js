7b06c48c9d707bfc44ff91674b77ba254e8d293a
krisrss <kristapsrss@gmail.com>
2021-05-05 21:09:34 +0300

    Make entire drink thumbnail clickable

--- src/components/DrinkThumbnail.js
+++ src/components/DrinkThumbnail.js
@@ -5,32 +5,32 @@ import '../css/DrinkThumbnail.css';
 
 const DrinkThumbnail = ({ drink, imageLoaded }) => {
     return (
-        <div className='DrinkThumbnail'>
-            <Link to={`/details/${drink.idDrink}`}>
+        <Link to={`/details/${drink.idDrink}`}>
+            <div className='DrinkThumbnail'>
                 <div className='image-wrapper'>
                     <img onLoad={imageLoaded} src={`${drink.strDrinkThumb}/preview`} alt="Drink" />
                 </div>
-            </Link>
 
-            <div className='info-wrapper'>
-                <div className='title'>
-                    {drink.strDrink}
-                </div>
-
-                <div className='info'>
-                    <div className='ingredients-tab'>
-                        <i class="fas fa-cocktail"></i>
-                        {` ${drink.ingredientCount[6]}`}
-                        <div className='ing-text'>ingredients</div>
+                <div className='info-wrapper'>
+                    <div className='title'>
+                        {drink.strDrink}
                     </div>
 
-                    <div className='stars-tab'>
-                        <div className='stars'>{setStarsFull(drink.makeDifficulty[0])}</div>
-                        <div className='stars-text'>difficulty</div>
+                    <div className='info'>
+                        <div className='ingredients-tab'>
+                            <i class="fas fa-cocktail"></i>
+                            {` ${drink.ingredientCount[6]}`}
+                            <div className='ing-text'>ingredients</div>
+                        </div>
+
+                        <div className='stars-tab'>
+                            <div className='stars'>{setStarsFull(drink.makeDifficulty[0])}</div>
+                            <div className='stars-text'>difficulty</div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 };
 
