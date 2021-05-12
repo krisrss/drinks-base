faea17275a159f269be18df46b6c63a32e3fd601
krisrss <kristapsrss@gmail.com>
2021-05-12 13:08:50 +0300

    Fix styling of Info bar

--- src/components/SearchTermBlock.js
+++ src/components/SearchTermBlock.js
@@ -4,28 +4,54 @@ import '../css/Tag.css';
 
 
 const SearchTermBlock = ({ searchTerm }) => {
+    const termQuantity = (Object.values(searchTerm).length);
     const searchTerms = Object.values(searchTerm).map((term, index) => {
-        if (index === 0) {
+        if (termQuantity === 1) {
             return (
                 <span key={index}>
-                    {`${term}`}
+                    <span className='termBold'>{term}</span>
                 </span>
             )
         }
-        else if (index === 1) {
-            return (
-                <span key={index}>
-                    {`, ${term}`}
-                </span>
-            )
+        else if (termQuantity === 2) {
+            if (index === 0) {
+                return (
+                    <span key={index}>
+                        <span className='termBold'>{term}</span>
+                    </span>
+                )
+            }
+            else if (index === 1) {
+                return (
+                    <span key={index}>
+                        <span> and </span> <span className='termBold'>{term}</span>
+                    </span>
+                )
+            }
         }
-        else if (index === 2) {
-            return (
-                <span key={index}>
-                    {` and ${term}`}
-                </span>
-            )
-        }
+        else if (termQuantity === 3) {
+            if (index === 0) {
+                return (
+                    <span key={index}>
+                        <span className='termBold'>{term}</span>
+                    </span>
+                )
+            }
+            else if (index === 1) {
+                return (
+                    <span key={index}>
+                        <span>, </span> <span className='termBold'>{term}</span>
+                    </span>
+                )
+            }
+            else if (index === 2) {
+                return (
+                    <span key={index}>
+                        <span> and </span><span className='termBold'>{term}</span>
+                    </span>
+                )
+            }
+        };
     })
 
     return (
