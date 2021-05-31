0d939a0fc3a00f4644f93670b03bb5da9f404183
krisrss <kristapsrss@gmail.com>
2021-05-31 21:19:15 +0300

    Display ingredients and instructions on details page

--- src/components/DrinkDetails.js
+++ src/components/DrinkDetails.js
@@ -22,22 +22,66 @@ const DrinkDetails = () => {
             setDrinkData(dataUpdated[0]);
         };
         getDrink();
-    }, [drinkId]);
+    }, []);
+
+
+
+    const ingredientsList = (data) => {
+
+        let ingredientsData = [];
+
+        for (let i = 1; i <= 15; i++) {
+            let ingr = data['strIngredient' + i];
+            let amount = data['strMeasure' + i];
+
+            if (ingr !== null && ingr !== undefined && ingr !== '') {
+                let ignWrap = [];
+                ignWrap.push(ingr)
+                ignWrap.push(amount)
+                ingredientsData.push(ignWrap)
+            }
+
+        }
+
+        return (ingredientsData);
+    };
+
+    const displayIngredients = ingredientsList(drinkData).map((ingredient) => {
+        return (
+            <div>
+                <span>{ingredient[0]}</span> - <span>{ingredient[1]}</span>
+            </div>
+        )
+    });
 
     return (
         <div>
             <Logo />
-            <div className='DrinkDetails'>
-                <div className='tags-list'>
-                    <span className='tag'>{drinkData.strCategory}</span>
-                    <span className='tag'>{drinkData.strAlcoholic}</span>
-                    <span className='tag'>{drinkData.strGlass}</span>
-                    <span className='tag'>{setStarsFull(drinkData.makeDifficulty === undefined ? [] : drinkData.makeDifficulty[0])}</span>
-                </div>
+            {Object.entries(drinkData).length !== 0 ?
+                <div className='DrinkDetails'>
+                    <div className='tags-list'>
+                        <span className='tag'>{drinkData.strCategory}</span>
+                        <span className='tag'>{drinkData.strAlcoholic}</span>
+                        <span className='tag'>{drinkData.strGlass}</span>
+                        <span className='tag'>{setStarsFull(drinkData.makeDifficulty === undefined ? [] : drinkData.makeDifficulty[0])}</span>
+                    </div>
 
-                <div className='title'>{drinkData.strDrink}</div>
-                <img alt='drink' src={drinkData.strDrinkThumb} />
-            </div>
+                    <div className='title'>{drinkData.strDrink}</div>
+
+                    <div className='content'>
+                        <div className='main-image'><img alt='drink' src={drinkData.strDrinkThumb} /></div>
+
+                        <div className='main-text'>
+                            <b>Ingredients:</b>
+                            {displayIngredients}
+                            <b>Instructions:</b>
+                            <p>{drinkData.strInstructions}</p>
+                        </div>
+
+                    </div>
+                </div>
+                : null
+            }
         </div>
 
     );
