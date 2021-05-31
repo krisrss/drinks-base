3974dc74f18a16a232232a58e167c72c27aac0c1
krisrss <kristapsrss@gmail.com>
2021-05-31 15:31:39 +0300

    Add stylesheet to drink details page

--- src/components/DrinkDetails.js
+++ src/components/DrinkDetails.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import Logo from '../components/Logo';
+import '../css/DrinkDetails.css';
 
 const DrinkDetails = () => {
     const { drinkId } = useParams();
@@ -20,9 +22,18 @@ const DrinkDetails = () => {
 
     return (
         <div>
-            <h1>{drinkData.strDrink}</h1>
-            <img alt='drink' src={drinkData.strDrinkThumb} />
+            <Logo />
+            <div className='DrinkDetails'>
+
+                <div className='title'>{drinkData.strDrink}</div>
+
+                {console.log(drinkData)}
+
+                <img alt='drink' src={drinkData.strDrinkThumb} />
+
+            </div>
         </div>
+
     );
 };
 
