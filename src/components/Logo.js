bd9e5cde439cf38eb18e26e4709b6d169f0f2dd7
krisrss <kristapsrss@gmail.com>
2021-06-12 13:47:37 +0300

    Add logo to pages

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import '../css/Logo.css';
-
+import mainLogo from '../images/logo.png';
 
 const Logo = () => {
     return (
         <div className='Logo'>
-            Drink Base Logo Place holder
+
+            <div className='img-wrapper'>
+                <img src={mainLogo} />
+            </div>
+
+
         </div>
     );
 };
