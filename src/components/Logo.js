78e717f7df78e84f2b105f847588478d811cfa4c
krisrss <kristapsrss@gmail.com>
2021-06-15 19:43:31 +0300

    Add hyperlink to logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import '../css/Logo.css';
 import mainLogo from '../images/logo.png';
+import { Link, useHistory } from 'react-router-dom';
 
 const Logo = () => {
+
+    const history = useHistory();
+
+    const setPath = () => {
+        if (history.location.pathname.includes('/ingredients')) {
+            return '/ingredients';
+        }
+        else {
+            return '/';
+        };
+    };
+
     return (
         <div className='Logo'>
-
             <div className='img-wrapper'>
-                <img src={mainLogo} />
+                <Link to={setPath()}>
+                    <img src={mainLogo} />
+                </Link>
             </div>
-
-
         </div>
     );
 };
