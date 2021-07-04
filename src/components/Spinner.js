c989e345ab7cf9a683525aab98f7c9c2efe8c18c
krisrss <kristapsrss@gmail.com>
2021-07-04 13:43:44 +0300

    Adjust spinner color and size

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import '../css/Spinner.css';
 
 const Spinner = () => {
     return (
-        <div className='spinner-border text-primary' role='status'>
+        <div className='spinner-border main-spinner' role='status'>
             <span className='sr-only'>Loading...</span>
         </div>
     );
