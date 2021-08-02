7764a3364ba15bdf8bb24092a83d5722c2d9ca0d
krisrss <kristapsrss@gmail.com>
2021-08-02 16:55:07 +0300

    Display input bar with common style

--- src/components/AutocompleteSearch.js
+++ src/components/AutocompleteSearch.js
@@ -1,8 +1,14 @@
 import React from "react";
+import '../css/SearchBar.css';
 
 const AutocompleteSearch = () => {
     return (
-        <div>AutoComplete bar</div>
+        <div className='SearchBar' >
+            <input type="text" />
+            <span className='button'>
+                SEARCH
+            </span>
+        </div >
     );
 }
 export default AutocompleteSearch;
