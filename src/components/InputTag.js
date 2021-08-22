c9745a29c6fa6b914ef1292c31c344d63ab0ae08
krisrss <kristapsrss@gmail.com>
2021-08-22 21:38:14 +0300

    Do not display dropdown when deleting tags

--- src/components/InputTag.js
+++ src/components/InputTag.js
@@ -2,8 +2,14 @@ import React from 'react';
 import '../css/InputTag.css';
 
 const InputTag = ({ tagName, deleteTags }) => {
+
+    const onClickHandler = (e) => {
+        e.stopPropagation();
+        deleteTags(tagName);
+    }
+
     return (
-        <span key={tagName} className='InputTag' onClick={() => deleteTags(tagName)}>
+        <span key={tagName} className='InputTag' onClick={onClickHandler}>
             {tagName}
         </span>
     )
